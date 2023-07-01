07f7c175c768eb3b907c6b3e798b4e9c9c476197
Riazanov <ryazanoffline@gmail.com>
2023-07-01 15:18:23 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int a = 7;
+    int b = 9;
+    a += b;
+    b = a - b;
+    a = a - b;
+        System.out.println(a);
+        System.out.println(b);
     }
+
 }
